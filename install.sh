495104befc79c30dd7e2d9614b1d4e43e7b69440
root <root@localhost.localdomain>
2016-03-14 15:48:21 +0800

    添加autojump

--- install.sh
+++ install.sh
@@ -3,5 +3,11 @@
 #  > Created Time: 2016年03月12日 星期六 22时57分35秒
 # ************************************************************************/
 #
+#################################################
 # install copydir for zsh
 sudo apt-get install xclip
+#-- autojump
+git clone git://github.com/joelthelion/autojump.git
+cd autojump
+./install.py
+
