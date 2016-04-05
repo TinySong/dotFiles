81a06cbe239f7785b676588b70564b758b55b0f4
root <root@localhost.localdomain>
2016-04-05 19:17:03 +0800

    add npm

--- install.sh
+++ install.sh
@@ -25,3 +25,13 @@ python -c "import sys; print sys.executable"
  pip install yapf
  # set yapf path in $PATH
 
+
+#npm for ubuntu
+http://www.cnblogs.com/seanlv/archive/2011/09/24/2189719.html
+
+# centos need download source code and compile, cannot npm install, 
+#    exec npm install -g npm to update npm
+# for javascript
+npm install -g npm
+npm install -g tern
+npm install -g js-beautify
